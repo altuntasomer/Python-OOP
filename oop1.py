662291fa625f4370c9fc4d410637e59e46e5635d
altuntasomer <altuntas.omer@yandex.com.tr>
2022-01-27 16:51:27 +0300

    updated oop1.py

--- oop1.py
+++ oop1.py
@@ -1,8 +1,18 @@
-class Human:
+class Car:
 
-    def speak(self):
-        print("hello")
+    def __init__(self, color, max_speed, hp, weight) -> None:
+        
+        self.color = color
+        self.max_speed = max_speed
+        self.hp = hp
+        self.weight = weight
 
-human = Human()
 
-human.speak()
+    def printInfo(self):
+
+        print("color is: {}, max speed is: {}, horse power is: {}, weight is: {}".format(self.color, self.max_speed, self.hp, self.weight))
+
+car1 = Car("red", 250, 280, 1540)
+car2 = Car("blue", 220, 180, 1240)
+car1.printInfo()
+car2.printInfo()
